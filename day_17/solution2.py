f212a53b643049da00bf1eceb4c4100c0e3526b4
Raphael <40037381+rk-exxec@users.noreply.github.com>
2021-12-19 22:01:48 +0100

    finish day 17

--- day_17/solution2.py
+++ day_17/solution2.py
@@ -43,53 +43,23 @@ def ycheck(pos, target):
     elif pos < y1:
         return 2
 
-def xcheck(pos, target, vel):
+def xcheck(pos, target):
     x1,x2 = target
-    if vel == 0:
-        return 2
-    elif x1 <= pos <= x2:
+    if x1 <= pos <= x2:
         return 1
     elif pos < x1:
         return 0
     elif pos > x2:
         return 2
 
-y_range = range(-800,800)
+y_range = range(-200,200)
 
-x_range = range(-100,800)
-
-ycandidates = []
-xcandidates = []
-
-for i in y_range:
-    vel = i
-    pos = 0
-    while True:
-        pos, vel = ystep(pos, vel)
-        if ycheck(pos,(y1,y2)) == 1:
-            ycandidates.append(i)
-            break
-        elif ycheck(pos,(y1,y2)) == 2:
-            break
-        
-            
-for i in x_range:
-    vel = i
-    pos = 0
-    while True:
-        pos, vel = xstep(pos, vel)
-        if xcheck(pos,(x1,x2), vel) == 1:
-            xcandidates.append(i)
-            break
-        elif xcheck(pos,(x1,x2), vel) == 2:
-            break
-
-
-#print (len(xcandidates),len(ycandidates))
+x_range = range(0,200)
 
 hits = np.zeros((x2-x1+1, y2-y1+1), dtype=np.int32)
-for ivelx in xcandidates:
-    for ively in ycandidates:
+candidates = []
+for ivelx in x_range:
+    for ively in y_range:
         posx = 0
         posy = 0
         velx = ivelx
@@ -98,12 +68,14 @@ for ivelx in xcandidates:
         while True:
             posx, velx = xstep(posx, velx)
             posy, vely = ystep(posy, vely)
-            if xcheck(posx,(x1,x2), velx) == 1 and ycheck(posy,(y1,y2)) == 1:
+            if xcheck(posx,(x1,x2)) == 1 and ycheck(posy,(y1,y2)) == 1:
+                candidates.append((ivelx, ively))
                 hits[posx-x1,posy-y1] += 1
                 break
-            elif xcheck(posx,(x1,x2), velx) == 2 or ycheck(posy,(y1,y2)) == 2:
+            elif xcheck(posx,(x1,x2)) == 2 or ycheck(posy,(y1,y2)) == 2:
                 break
 
 plt.imshow(hits.T, cmap='hot')
 plt.show()
+print(candidates)
 print(hits.sum())
